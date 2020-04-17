237c31a7b79825a59f79faa89cb413285231aa58
Crihan13na <crihan.anastasia@gmail.com>
2020-04-17 18:30:49 -0500

    OnCLick EVent to update graphs

--- static/js/test.js
+++ static/js/test.js
@@ -170,3 +170,45 @@ function updateLegend(time, stationCount, icon) {
  };
 }));
 }));
+
+
+function stateChange() {
+  am4core.disposeAllCharts();
+  let curState = this.value;
+ HenockChart(curState);
+
+  JJChart(curState);
+
+
+}
+
+stateSelector.on("change", stateChange);
+
+
+//function for when the user selects a state
+function optionChanged(newCity){
+  //functions for drawing graphs here
+  cityUpdate(newCity);
+}
+
+//function for initial landing page
+function initDashboard(){
+  var stateSelector = d3.select("#selDataset1");
+
+  d3.json(salesUrl).then(function(sales){
+    salesRadialData = sales;
+  HenockChart("...city...");
+  });  
+  d3.json(deathsUrl).then((deaths)=>{
+    deathsData = deaths;
+   JJChart("...city...");
+    var stateName = [];
+    for (var a = 0; a<statesData.features.length; a++){
+      stateName.push(statesData.features[a].properties.city_name);
+    }
+    stateName.forEach((stateSelect)=>{
+      stateSelector.append("option")
+        .text(stateSelect)
+        .property("value", stateSelect)
+    });
+    var stateSelect = stateName[0];
